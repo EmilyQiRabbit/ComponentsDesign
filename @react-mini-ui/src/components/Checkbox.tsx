5f6a3197d7249a5230a8fb48855146b409054d04
EmilyQiRabbit <LiuYQEmily@163.com>
2019-11-12 19:13:14 +0800

    feat: add animations

--- @react-mini-ui/src/components/Checkbox.tsx
+++ @react-mini-ui/src/components/Checkbox.tsx
@@ -12,29 +12,34 @@ const StyledCheckbox = styled.div`
   width: 16px;
   height: 16px;
   border: 1px solid #d9d9d9;
-  border-radius: 2px;
+  border-radius: 10px;
   cursor: pointer;
   position: relative;
+  border-collapse: separate;
+  transition: all .3s;
   &:hover {
     border-color: #1890ff;
+  }
+  &:after{
+    position: absolute;
+    top: 49%;
+    left: 20%;
+    display: table;
+    width: 5.71428571px;
+    height: 9.14285714px;
+    border: 2px solid #fff;
+    border-top: 0;
+    border-left: 0;
+    transform: rotate(45deg) scale(1) translate(-50%, -50%);
+    opacity: 0;
+    transition: all .2s cubic-bezier(.12, .4, .29, 1.46) .1s;
+    content: ' ';
   }
   &.checked {
     background-color: #1890ff;
     border-color: #1890ff;
     &:after{
-      position: absolute;
-      top: 49%;
-      left: 20%;
-      display: table;
-      width: 5.71428571px;
-      height: 9.14285714px;
-      border: 2px solid #fff;
-      border-top: 0;
-      border-left: 0;
-      transform: rotate(45deg) scale(1) translate(-50%, -50%);
       opacity: 1;
-      transition: all .2s cubic-bezier(.12, .4, .29, 1.46) .1s;
-      content: ' ';
     }
   }
 `
