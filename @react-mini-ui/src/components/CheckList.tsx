c36a95d62dc53f3fd9674dbf3d09c776a6c72318
EmilyQiRabbit <LiuYQEmily@163.com>
2020-02-11 22:35:13 +0800

    chore: 更新文件解构

--- @react-mini-ui/src/components/CheckList.tsx
+++ @react-mini-ui/src/components/CheckList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import moment from 'moment';
 import { PureComponent } from 'react';
+import { Checkbox } from './CheckItem';
 
 const titleOptions = [ '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖', '拾'];
 const plainOptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
@@ -231,56 +232,3 @@ const Root = styled.div`
     display: none;
   }
 `;
-
-// LightCheckbox
-interface CheckboxProps {
-  disabled: boolean;
-  checked: boolean;
-  children: any;
-  onCheckboxClick: (checked: boolean) => void
-}
-
-const Checkbox = (props: CheckboxProps) => {
-  const { children, disabled, checked } = props;
-  const onCheckboxClick = () => {
-    props.onCheckboxClick(!checked)
-  }
-  return <StyledSpan 
-    className={`${disabled && 'disabled'} ${checked && 'checked'}`}
-    onClick={onCheckboxClick}
-  >{children}</StyledSpan>
-}
-
-const StyledSpan = styled.span`
-  font-size: 12px;
-  width: 22px;
-  height: 22px;
-  line-height: 22px;
-  display: inline-block;
-  text-align: center;
-  border-radius: 2px;
-  margin: 3px;
-  color: rgba(0, 0, 0, 0.85);
-  cursor: pointer;
-  text-size-adjust: 100%;
-  &.disabled {
-    color: #a5a5a5;
-    cursor: not-allowed;
-  }
-  &.checked {
-    color: white;
-    background: #6da0e3
-  }
-  &:hover {
-    &:not(.checked) {
-      &:not(.disabled) {
-        background: rgba(109, 160, 227, 0.2);
-        color: #6da0e3;
-      }
-    }
-  }
-  &:last-child {
-    padding-right: 2px;
-  }
-`
-// 这里的 padding-right 就是为了适配 10 这个数字看起来稍微有些不居中的问题
